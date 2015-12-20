b5061b88cdf7480d77f0879653560985583b7823
timonov <timonova.a@gmail.com>
2015-12-20 14:49:54 +0300

    added calculator and plotter accuracy score; added description results

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import numpy as np
-from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier as rfc
-from sklearn.ensemble import GradientBoostingClassifier as gbc
-from sklearn.naive_bayes import BernoulliNB as bnb
+from sklearn.ensemble import RandomForestClassifier as Rfc
+from sklearn.ensemble import GradientBoostingClassifier as Gbc
+from sklearn.naive_bayes import BernoulliNB as Bnb
 from sklearn.cross_validation import KFold
-import plot
+from plot import *
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+# download matrix ############################################################################################
 
 
 def swap(index1, index2, iterable):
@@ -20,27 +23,21 @@ def download_matrix(path):
     swap(0, 6, matrix_from_file)
     return matrix_from_file
 
+##############################################################################################################
 
-def prepare_matrix_for_feature_engineering(matrix_from_file):
+
+def prepare_matrix_for_feature_engineering(matrix_from_file, step):
     x_all = matrix_from_file[:, 1:]
     y_all = matrix_from_file[:, 0]
-    x_train = matrix_from_file[0:150, 1:]
-    y_train = matrix_from_file[0:150, 0]
-    x_test = matrix_from_file[150:194, 1:]
-    y_test = matrix_from_file[150:194, 0]
+    x_train = matrix_from_file[0:step, 1:]
+    y_train = matrix_from_file[0:step, 0]
+    x_test = matrix_from_file[step:194, 1:]
+    y_test = matrix_from_file[step:194, 0]
     return x_train, y_train, x_test, y_test, x_all, y_all
-
-# from sklearn.naive_bayes import GaussianNB as gnb
-# def gaussian_nb(x_train, y_train, x_test, y_test):
-#     model = gnb()
-#     model.fit(x_train, y_train)
-#     expected = y_test
-#     predicted = model.predict(x_test)
-#     return expected, predicted
 
 
 def naive_bayes(x_train, y_train, x_test, y_test):
-    model = bnb()
+    model = Bnb()
     model.fit(x_train, y_train)
     expected = y_test
     predicted = model.predict(x_test)
@@ -48,7 +45,7 @@ def naive_bayes(x_train, y_train, x_test, y_test):
 
 
 def random_forest_classifier(x_train, y_train, x_test, y_test):
-    model = rfc(n_estimators=10, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1,
+    model = Rfc(n_estimators=10, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1,
                 min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, bootstrap=True,
                 oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
     model.fit(x_train, y_train)
@@ -57,24 +54,16 @@ def random_forest_classifier(x_train, y_train, x_test, y_test):
     return expected, predicted
 
 
-# def random_forest_classifier(x_train, y_train, x_test, y_test):
-#     model = rfc(n_estimators=100)
-#     model.fit(x_train, y_train)
-#     predicted = model.predict(x_test)
-#     expected = y_test
-#     # print expected
-#     # print predicted
-#     return expected, predicted
-
-
 def gradient_boosting_classifier(x_train, y_train, x_test, y_test):
-    model = gbc(loss='deviance', learning_rate=0.1, n_estimators=10, subsample=1.0, min_samples_split=2,
+    model = Gbc(loss='deviance', learning_rate=0.1, n_estimators=10, subsample=1.0, min_samples_split=2,
                 min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=1, init=None, random_state=None,
                 max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False)
     model.fit(x_train, y_train)
     expected = y_test
     predicted = model.predict(x_test)
     return expected, predicted
+
+##############################################################################################################
 
 
 def run_cross_validation(x, y, clf_class, **kwargs):
@@ -93,32 +82,51 @@ def run_cross_validation(x, y, clf_class, **kwargs):
 def accuracy(y_true, y_prediction):
     return np.mean(y_true == y_prediction)
 
+##############################################################################################################
 
 if __name__ == "__main__":
     _matrix_from_file = download_matrix("./data.txt")
-    _x_train, _y_train, _x_test, _y_test, _x_all, _y_all = prepare_matrix_for_feature_engineering(_matrix_from_file)
 
-    scaler = StandardScaler()
-    _x_all = scaler.fit_transform(_x_all)
+# section one: increase _x_train and calculate accuracy score
+    _x_train_steps = []
+    accuracy_score_bnb = []
+    accuracy_score_rfc = []
+    accuracy_score_gbc = []
+    for i in range(30, 160, 20):
+        _x_train_steps.append(i)
+        _x_train, _y_train, _x_test, _y_test, _x_all, _y_all = \
+            prepare_matrix_for_feature_engineering(_matrix_from_file, i)
+        expected_bnb, predicted_bnb = naive_bayes(_x_train, _y_train, _x_test, _y_test)
+        expected_rfc, predicted_rfc = random_forest_classifier(_x_train, _y_train, _x_test, _y_test)
+        expected_gbc, predicted_gbc = gradient_boosting_classifier(_x_train, _y_train, _x_test, _y_test)
 
+        accuracy_score_bnb.append(accuracy_score(expected_bnb, predicted_bnb))
+        accuracy_score_rfc.append(accuracy_score(expected_rfc, predicted_rfc))
+        accuracy_score_gbc.append(accuracy_score(expected_gbc, predicted_gbc))
+    default_plot_report(_x_train_steps, accuracy_score_bnb, accuracy_score_rfc, accuracy_score_gbc)
+
+# section two: plot confusion matrix of the classifier
+    _x_train, _y_train, _x_test, _y_test, _x_all, _y_all = \
+        prepare_matrix_for_feature_engineering(_matrix_from_file, 90)
     expected_bnb, predicted_bnb = naive_bayes(_x_train, _y_train, _x_test, _y_test)
     expected_rfc, predicted_rfc = random_forest_classifier(_x_train, _y_train, _x_test, _y_test)
     expected_gbc, predicted_gbc = gradient_boosting_classifier(_x_train, _y_train, _x_test, _y_test)
 
-    confusion_matrix_bnb = metrics.confusion_matrix(expected_bnb, predicted_bnb)
-    plot.plot_classification_report(confusion_matrix_bnb)
-    confusion_matrix_frc = metrics.confusion_matrix(expected_rfc, predicted_rfc)
-    plot.plot_classification_report(confusion_matrix_frc)
-    confusion_matrix_gbc = metrics.confusion_matrix(expected_gbc, predicted_gbc)
-    plot.plot_classification_report(confusion_matrix_gbc)
+    confusion_matrix_bnb = confusion_matrix(expected_bnb, predicted_bnb)
+    confusion_matrix_frc = confusion_matrix(expected_rfc, predicted_rfc)
+    confusion_matrix_gbc = confusion_matrix(expected_gbc, predicted_gbc)
+    plot_classification_report(confusion_matrix_bnb)
+    plot_classification_report(confusion_matrix_frc)
+    plot_classification_report(confusion_matrix_gbc)
 
-    # TODO: plot classification report
-    # classification_report = metrics.classification_report(expected_gnb, predicted_gnb)
-    # print classification_report
-
+# section three: run cross validation and print results
+    _x_all = _matrix_from_file[:, 1:]
+    _y_all = _matrix_from_file[:, 0]
+    scaler = StandardScaler()
+    _x_all = scaler.fit_transform(_x_all)
     print "RandomForestClassifier:"
-    print "%.3f" % accuracy(_y_all, run_cross_validation(_x_all, _y_all, rfc))
+    print "%.3f" % accuracy(_y_all, run_cross_validation(_x_all, _y_all, Rfc))
     print "BernoulliNB:"
-    print "%.3f" % accuracy(_y_all, run_cross_validation(_x_all, _y_all, bnb))
+    print "%.3f" % accuracy(_y_all, run_cross_validation(_x_all, _y_all, Bnb))
     print "GradientBoostingClassifier:"
-    print "%.3f" % accuracy(_y_all, run_cross_validation(_x_all, _y_all, gbc))
+    print "%.3f" % accuracy(_y_all, run_cross_validation(_x_all, _y_all, Gbc))
--- plot.py
+++ plot.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt  # 2d plotting library
+import matplotlib.pyplot as plt
 import pylab as pl
 
 
-def plot_classification_report(cm, title='Classification report ', with_avg_total=False):
+def plot_classification_report(cm):
     labels = ['class 0', 'class 1', 'class 2', 'class 3', 'class 4', 'class 5', 'class 6', 'class 7', 'class 8']
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -15,3 +15,13 @@ def plot_classification_report(cm, title='Classification report ', with_avg_tota
     pl.ylabel('True')
     pl.show()
 
+
+def default_plot_report(_x_train_steps, accuracy_score_bnb, accuracy_score_rfc, accuracy_score_gbc):
+    pl.title("Dependence the method's accuracy of the value of training sample")
+    pl.xlabel("_x_train_steps")
+    pl.ylabel("accuracy_score")
+    plt.plot(_x_train_steps, accuracy_score_bnb, label="NaiveBayes")
+    plt.plot(_x_train_steps, accuracy_score_rfc, label="RandomForestClassifier")
+    plt.plot(_x_train_steps, accuracy_score_gbc, label="GradientBoostingClassifier")
+    pl.legend(loc="upper left")
+    pl.show()
